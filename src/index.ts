102f52ee74329b02d4a956dd39f6768f97fe52ca
Alex Currie-Clark <acurrieclark@gmail.com>
2022-12-15 15:20:22 +0000

    Added an extra data field to see how the timing of the error changes

--- src/index.ts
+++ src/index.ts
@@ -8,12 +8,19 @@ type DocStructure = {
   counter: {
     value: number;
     updatedAt: number;
+    name: string;
   };
 };
 
 const initialState: DocStructure = {
-  counter: { updatedAt: 0, value: 0 }
+  counter: { updatedAt: 0, value: 0, name: "a" }
 };
+
+const shallowCompare = (obj1: Record<string, any>, obj2: Record<string, any>) =>
+    Object.keys(obj1).length === Object.keys(obj2).length &&
+    Object.keys(obj1).every(key =>
+        obj2.hasOwnProperty(key) && obj1[key] === obj2[key]
+    );
 
 const doc = Automerge.init<DocStructure>();
 
@@ -25,15 +32,19 @@ const container1 = new MemoryDocContainer<DocStructure>(change<DocStructure>(doc
 const container2 = new SavedDocContainer<DocStructure>();
 
 try {
-  for (let i = 0; i < 50; i++) {
+  for (let i = 0; i < 100; i++) {
     container1.change((doc) => {
       doc.counter.value++;
+      doc.counter.name += "a";
       doc.counter.updatedAt = Date.now();
     });
-  
-    console.log(container1.getDoc().counter.value);
     
     container1.syncWithContainer(container2);
+
+    console.log("at count", container1.getDoc().counter.value, "the document values are", shallowCompare({...
+      container1
+      .getDoc().counter
+    }, {...container2.getDoc().counter}) ? 'equal' : 'not equal (' + JSON.stringify(container1.getDoc()) + ' vs ' + JSON.stringify(container2.getDoc()) + ")");
   }
 } catch (e) {
   console.error(e, "when syncing count to", container1.getDoc().counter.value);
